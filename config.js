16876ad9b15043c1eef4e582d6c8e09e2c5a6507
Paweł Koleśnik <pawelkolesnik@wp.pl>
2018-03-26 20:44:16 +0200

    do task

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
 	'GOOGLE_CLIENT_ID': '878509954605-gfujehnin9k6ikv9tmgj2n8rji4l7b1l.apps.googleusercontent.com',
 	'GOOGLE_CLIENT_SECRET': '-J0alrxmTwi1Fw1KmLvefbX-',
 	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
